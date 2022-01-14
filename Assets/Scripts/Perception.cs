a30215f6b960af27be713f1d8f393b6fb4b640e9
mmargolin <mmargolin@student.neumont.edu>
2022-01-14 00:06:01 -0700

    AAA
    AAA

---
+++ Assets/Scripts/Perception.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Perception : MonoBehaviour
+{
+    public string tagName;
+
+    public abstract GameObject[] GetGameObjects();
+
+        
+
+}
